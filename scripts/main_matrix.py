b79e4f6d1783b062e21478d3363f361883298711
Roy Yali <ryali93@gmail.com>
2018-09-03 21:21:48 -0500

    Longitud correlativa y graficos

--- scripts/main_matrix.py
+++ scripts/main_matrix.py
@@ -3,6 +3,7 @@
 
 from _karasiev import *
 from ehidropy import *
+from longcor import *
 
 # Variables globales
 BUDYKO_FIELD = 'BDK'
@@ -18,6 +19,8 @@ ksv.set_epsg(EPSG)
 # Se obtiene el codigo de las cuencas
 # cuencas = set([i[0] for i in arcpy.da.SearchCursor(EHIDROMETRICA, [CODCUENCA], "%s IN ('004984918')" % CODCUENCA)])
 cuencas = set([i[0] for i in arcpy.da.SearchCursor(EHIDROMETRICA, [CODCUENCA], "%s IS NOT NULL" % CODCUENCA)])
+cuencas = ['000131804']
+
 
 for i in cuencas:
     print i
@@ -34,13 +37,21 @@ for i in cuencas:
     matrix_co = ksv.mx_correlation(matrix_gr)
     matrix_si = ksv.mx_significance(matrix_co, YEARS_OF_READER)
 
+    # Crear carpeta
+    pathMatrix = os.path.join(MATRIX_DIR, i)
+    if not os.path.exists(pathMatrix):
+        os.makedirs(pathMatrix)
+
     # Expotando matrices como archivos *.csv
-    matrix_da.to_csv(os.path.join(MATRIX_DIR, 'matrix_da_%s.csv' % i))
-    matrix_di.to_csv(os.path.join(MATRIX_DIR, 'matrix_di_%s.csv' % i))
-    matrix_gr.to_csv(os.path.join(MATRIX_DIR, 'matrix_gr_%s.csv' % i))
-    matrix_co.to_csv(os.path.join(MATRIX_DIR, 'matrix_co_%s.csv' % i))
-    matrix_si.to_csv(os.path.join(MATRIX_DIR, 'matrix_si_%s.csv' % i))
+    matrix_da.to_csv(os.path.join(pathMatrix, 'matrix_da_%s.csv' % i))
+    matrix_di.to_csv(os.path.join(pathMatrix, 'matrix_di_%s.csv' % i))
+    matrix_gr.to_csv(os.path.join(pathMatrix, 'matrix_gr_%s.csv' % i))
+    matrix_co.to_csv(os.path.join(pathMatrix, 'matrix_co_%s.csv' % i))
+    matrix_si.to_csv(os.path.join(pathMatrix, 'matrix_si_%s.csv' % i))
 
     # Probando la generacion de la matrices de radio correlativo (lo)
     matrix_rc = make_matrix_radio_correlative(mx_gr=matrix_gr, mx_di=matrix_di, mx_co=matrix_co, mx_si=matrix_si)
-    matrix_rc.to_csv(os.path.join(MATRIX_DIR, 'matrix_rc_%s.csv' % i))
+    matrix_rc.to_csv(os.path.join(pathMatrix, 'matrix_rc_%s.csv' % i))
+
+    # Realizar los graficos
+    get_idrc(i)
